c46255bdb103d25c6e61a84d1b78f3958c241539
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-18 01:24:20 +1000

    update view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,13 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
+
+  ALPHA = %w(S H J T C N J R F K B)
+  
   def new
-    @letters = ['jsck', 'john']
+    @letters = Array.new(5) {ALPHA.sample}
+    @letters += Array.new(5) { (('A'..'Z').to_a - ALPHA).sample}
+    @letters.shuffle!
   end
 
   def score
